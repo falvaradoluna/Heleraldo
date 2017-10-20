688044d44d8a6f62dd437eaa0fb2cc6c8d8a83ef
juanobrach <juanobrach@gmail.com>
2017-10-20 11:53:07 -0500

    Elimino controlDepositoIva

--- app/static/AngularJS/ControlDepositos/controlDepositosDirectives.js
+++ app/static/AngularJS/ControlDepositos/controlDepositosDirectives.js
@@ -1,24 +1,11 @@
 var ctrDepPath = 'AngularJS/ControlDepositos/';
 
-registrationModule.directive('ctrdepBancoFilter', function() {
-    return {
-        restrict: 'E',
-        templateUrl: ctrDepPath + 'controlDepositosBancoFilter.html'
-    };
-})
-.directive('ctrdepBancoTable', function(){
-    return {
-        restrict: 'E',
-        templateUrl: ctrDepPath + 'controlDepositosBancoTable.html'
-    };
-})
-.directive('ctrdepCarteraFilter', function() {
+registrationModule.directive('ctrdepCarteraFilter', function() {
     return {
         restrict: 'E',
         templateUrl: ctrDepPath + 'controlDepositosCarteraFilter.html'
     };
-})
-.directive('ctrdepCarteraFilterUser', function() {
+}).directive('ctrdepCarteraFilterUser', function() {
     return {
         restrict: 'E',
         templateUrl: ctrDepPath + 'controlDepositosCarteraFilterUser.html'
