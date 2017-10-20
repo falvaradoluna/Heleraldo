1b5a4f6f5eed06c383424b06b18a1ead7c0a2b00
juanobrach <juanobrach@gmail.com>
2017-10-20 17:37:52 -0500

    Elimino directivas en desuso

--- app/static/AngularJS/ControlDepositos/controlDepositosDirectives.js
+++ app/static/AngularJS/ControlDepositos/controlDepositosDirectives.js
@@ -1,21 +1,6 @@
 var ctrDepPath = 'AngularJS/ControlDepositos/';
 
-registrationModule.directive('ctrdepCarteraFilter', function() {
-    return {
-        restrict: 'E',
-        templateUrl: ctrDepPath + 'controlDepositosCarteraFilter.html'
-    };
-}).directive('ctrdepCarteraFilterUser', function() {
-    return {
-        restrict: 'E',
-        templateUrl: ctrDepPath + 'controlDepositosCarteraFilterUser.html'
-    };
-}).directive('ctrdepReferenciaDetail', function() {
-    return {
-        restrict: 'E',
-        templateUrl: ctrDepPath + 'controlDepositosReferenciaDetail.html'
-    };
-}).directive('ctrdepReferenciaTableHeraldo', function() {
+registrationModule.directive('ctrdepReferenciaTableHeraldo', function() {
     return {
         restrict: 'E',
         templateUrl: ctrDepPath + 'controlDepositosReferenciaTableHeraldo.html'
